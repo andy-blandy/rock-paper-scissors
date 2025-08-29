ff3597017ef7a71c8f05c6fd6522a2f7719d6e26
andy-blandy <shsandrewdaniels@gmail.com>
2025-08-29 18:34:47 -0400

    Added UI

--- javascript.js
+++ javascript.js
@@ -1,4 +1,5 @@
 const options = ["rock", "paper", "scissors"];
+const resultsDiv = document.querySelector(".results");
 
 function getComputerChoice() {
     let num = Math.floor(Math.random() * options.length);
@@ -18,48 +19,64 @@ function getHumanChoice() {
     }
 }
 
-function playGame(){
-    let humanScore = 0;
-    let computerScore = 0;
+function playRound(humanChoice, computerChoice){
+    let didPlayerWin = false;
+    let winner = "";
+    let loser = "";
+    let isDraw = false;
 
-    function playRound(humanChoice, computerChoice){
-        let didPlayerWin = false;
-        let winner = "";
-        let loser = "";
-
-        if (humanChoice == computerChoice){
-            console.log("Draw! Both players chose " + humanChoice);
-        } else if (humanChoice == "rock" && computerChoice == "scissors" ||
-                humanChoice == "paper" && computerChoice == "rock" ||
-                humanChoice == "scissors" && computerChoice == "paper") {
-            didPlayerWin = true;
-            winner = humanChoice;
-            loser = computerChoice;
-            humanScore++;
-        } else {
-            didPlayerWin = false;
-            winner = computerChoice;
-            loser = humanChoice;
-            computerScore++;
-        }
-
-        let winningMsg = "";
-        if (didPlayerWin) {
-            winningMsg = "You win!";
-        } else {
-            winningMsg = "You lose!";
-        }
-
-        winningMsg += " " + winner + " beats " + loser + "!";
-        console.log(winningMsg);
+    if (humanChoice == computerChoice){
+        isDraw = true;
+    } else if (humanChoice == "rock" && computerChoice == "scissors" ||
+            humanChoice == "paper" && computerChoice == "rock" ||
+            humanChoice == "scissors" && computerChoice == "paper") {
+        didPlayerWin = true;
+        winner = humanChoice;
+        loser = computerChoice;
+        //humanScore++;
+    } else {
+        didPlayerWin = false;
+        winner = computerChoice;
+        loser = humanChoice;
+        //computerScore++;
     }
 
-    do {
-        let humanSelection = getHumanChoice();
-        let computerSelection = getComputerChoice();
+    let winningMsg = "";
+    if (didPlayerWin) {
+        winningMsg = "You win!";
+    } else {
+        winningMsg = "You lose!";
+    }
 
-        playRound(humanSelection, computerSelection);
-    } while (humanScore < 5 || computerScore < 5);
+    winningMsg += " " + winner + " beats " + loser + "!";
+
+    if (isDraw) {
+        winningMsg = "Draw! Both players chose " + humanChoice;
+    }
+    resultsDiv.textContent = winningMsg;
 }
 
-playGame();
+const btns = document.querySelectorAll("button");
+btns.forEach((button) => {
+    button.addEventListener("click", function (e) {
+        let playerChoice = e.target.id;
+        let compChoice = getComputerChoice();
+        playRound(playerChoice, compChoice);
+    });
+});
+
+// function playGame(){
+//     let humanScore = 0;
+//     let computerScore = 0;
+
+
+
+//     do {
+//         let humanSelection = getHumanChoice();
+//         let computerSelection = getComputerChoice();
+
+//         playRound(humanSelection, computerSelection);
+//     } while (humanScore < 5 || computerScore < 5);
+// }
+
+// playGame();
